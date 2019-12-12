e406a002944fe33c7990bf81d4a45d06d99fa5fd
Nick Nichols <nick.nichols@rate.com>
2019-12-12 09:10:34 -0600

    Base terraform

---
+++ deploy/terraform/main.tf
@@ -0,0 +1,8 @@
+variable "example_app_name" {
+  description = "Name of the Heroku app provisioned as an example"
+}
+
+resource "heroku_app" "example" {
+  name   = "${var.example_app_name}"
+  region = "us"
+}
