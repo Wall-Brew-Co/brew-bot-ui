45547d99dba42f31475d0c51b18cee79cb4a843e
Nick Nichols <nick.nichols@rate.com>
2019-12-13 16:20:46 -0600

    Bump dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/nnichols/brew-bot-ui"
   :license {:name "Eclipse Public License v1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [compojure "1.6.1"]
-                 [ring/ring-jetty-adapter "1.7.1"]
+                 [ring/ring-jetty-adapter "1.8.0"]
                  [ring/ring-defaults "0.3.2"]
                  [ring-logger "1.0.1"]]
   :min-lein-version "2.0.0"
