d682c8933ca902791952cf17a921a432ba501ca2
Nick Nichols <nick.nichols@rate.com>
2020-08-23 09:12:27 -0500

    Appropriately raise HTTP status codes in error pages

--- src/clj/brew_bot_ui/http/html.clj
+++ src/clj/brew_bot_ui/http/html.clj
@@ -19,24 +19,51 @@
 (def fingerprint
   (str (c/to-long (t/now))))
 
+(defn add-default-page-values
+  "Adds default parameters to HTML template files"
+  [params]
+  (merge
+   {:fingerprint         fingerprint
+    :google-analytics-id config/google-analytics-id
+    :csrf-token          *anti-forgery-token*
+    :servlet-context     *app-context*}
+   params))
+
 (defn render
-  "renders the HTML template located relative to resources/public"
+  "Renders the HTML template located relative to resources/public with a 200 status code."
   [template & [params]]
-  (ring/content-type
-   (ring/ok
-    (parser/render-file
-     template
-     (assoc params
-            :fingerprint         fingerprint
-            :page                template
-            :google-analytics-id config/google-analytics-id
-            :csrf-token          *anti-forgery-token*
-            :servlet-context     *app-context*)))
-   "text/html; charset=utf-8"))
+  (let [render-params (assoc params :page template)
+        render-opts   (add-default-page-values render-params)]
+    (ring/content-type (ring/ok (parser/render-file template render-opts)) "text/html; charset=utf-8")))
 
 (defn index [] (render "index.html"))
-(defn not-authorized [] (render "403.html"))
-(defn not-found [] (render "404.html"))
-(defn server-error [] (render "500.html"))
 
 #_(defn example [] (render "example.html" {:title "Wall Brew"}))
+
+(defn error-page
+  "error-details should be a map containing the following keys:
+   :status - The HTTP status code.
+   :title - A title for the error page
+   :message - An error message to display to the user.
+   :error-page - A .html file in resources/public to display. Defaults to error.html
+
+   Returns a response map with the error page as the body and the status specified by the status key"
+  [{:keys [error-page title message] :as error-details}]
+  (let [title       (or title "Internal Server Error")
+        message     (or message "Something went wrong. Please try again later.")
+        error-page  (or error-page "error.html")
+        render-opts (-> error-details
+                        (dissoc :error-page)
+                        (assoc :title title :message message)
+                        add-default-page-values)]
+    {:status  (:status error-details)
+     :headers {"Content-Type" "text/html; charset=utf-8"}
+     :body    (parser/render-file error-page render-opts)}))
+
+(defn not-found
+  []
+  (error-page {:error-page "404.html" :status 404}))
+
+(defn not-authorized
+  []
+  (error-page {:status 403 :title "Not Authorized" :message "Invalid credentials detected."}))
