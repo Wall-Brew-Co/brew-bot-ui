d152a6e0616103a19fa7fe336ef3b5def87455b9
nnichols <nichols1991@gmail.com>
2020-11-03 08:00:38 -0600

    Add basic card element to hold each ingredient type, basic layout/classes for recipe builder

--- src/cljs/brew_bot_ui/recipe_builder/layout.cljs
+++ src/cljs/brew_bot_ui/recipe_builder/layout.cljs
@@ -3,10 +3,17 @@
             [reagent.core :as r]
             [re-frame.core :as rf]))
 
+(defn recipe-facts
+  []
+  [:div {:class (cu/join-classes "rounded-lg" "bg-gray-100" "divide-y" "divide-gray-400" "sm:w-full" "md:w-full" "lg:w-2/3" "xl:w-2/3")
+         :style {:padding "5px" :margin "auto" :min-width "300px"}}
+   [:div {:class (cu/join-classes "text-center" "py-2")}
+    [:h4 "Recipe Name"]]])
+
 (defn ingredient-box
   [ingredient-type]
-  [:div {:class (cu/join-classes "rounded-lg" "bg-gray-100" "divide-y" "divide-gray-400" "flex-none")
-         :style {:padding "5px" :margin "5px" :min-width "300px"}}
+  [:div {:class (cu/join-classes "rounded-lg" "bg-gray-100" "divide-y" "divide-gray-400" "px-2" "sm:w-full" "md:w-full" "lg:w-5/12" "xl:w-5/12" "sm:my-2" "md:my-2" "lg:m-2" "xl:m-2")
+         :style {:padding "5px" :min-width "300px"}}
    [:div {:class (cu/join-classes "text-center" "py-2")}
     [:h3 ingredient-type]]
    [:div {:class (cu/join-classes "text-base" "py-2")}
@@ -14,13 +21,15 @@
    [:div {:class (cu/join-classes "text-sm" "py-2")}
     [:p "This is the footer"]]])
 
-
 (defn main-panel
   []
   (let [current-page (rf/subscribe [:current-page])]
     (fn []
-      [:div
-       [:h1 (str "Hello there: " @current-page)]
-       [:div {:class (cu/join-classes "flex" "flex-wrap" "sm:flex-col" "md:flex-col" "lg:flex-row" "xl:flex-row")}
+      [:div {:class (cu/join-classes "justify-center")
+             :style {:width "95%"}}
+       [:div [recipe-facts]]
+       [:div {:class (cu/join-classes "flex" "flex-wrap" "sm:flex-col" "md:flex-col" "lg:flex-row" "xl:flex-row" "justify-center")}
          [ingredient-box "Fermentables"]
-         [ingredient-box "Hops"]]])))
+         [ingredient-box "Hops"]
+         [ingredient-box "Yeasts"]
+         [ingredient-box "Miscellaneous Ingredients"]]])))
